5ae1bf7aca18a645c86196b20486c5218ecb5b62
Jason Wadsworth <jdwadsworth@gmail.com>
2010-03-13 22:27:03 -0500

    Fixed broken require statement

--- lib/librarything/shared_requires.rb
+++ lib/librarything/shared_requires.rb
@@ -1,4 +1,4 @@
 require 'httparty'
 require 'nokogiri'
 require File.expand_path(File.dirname(__FILE__) + "/errors")
-require File.expand_path(File.dirname(__FILE__) + "/base")
+require File.expand_path(File.dirname(__FILE__) + "/resource")
